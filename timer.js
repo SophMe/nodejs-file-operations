414d31bdd79d8ea57575fbaa02981ab00cfc7b14
SophMe <116891325+SophMe@users.noreply.github.com>
2024-01-09 15:57:36 +0100

    add function create + write file

--- timer.js
+++ timer.js
@@ -26,3 +26,37 @@ function sleep(ms) {
 timer();
 
 // run in terminal with "node timer.js"
+
+// create file with random numbers
+// https://nodejs.org/en/learn/manipulating-files/writing-files-with-nodejs
+
+const fs = require('fs');
+
+function getRandomNumber(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+const randomNumbers = [];
+for (let i = 0; i < 1000; i++) {
+  randomNumbers.push(getRandomNumber(1, 2000));
+}
+const content = randomNumbers.join(', ');
+
+fs.writeFile('random_numbers.txt', content, (err) => {
+  if (err) {
+    console.error(err);
+  } else {
+    console.log('File written successfully');
+  }
+  console.log(content);
+});
+
+// fs.readFile('random_numbers.txt', 'utf8', (err, data) => {
+//   if (err) {
+//     console.error(err);
+//   } else {
+//     const numbersArray = data.split(',');
+//     const numberOfNumbers = numbersArray.length;
+//     console.log(`There are ${numberOfNumbers} numbers in the file: `);
+//   };
+// });
