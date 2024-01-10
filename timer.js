2b6ac6c3d02f396b616b3eb47df3b2c08daf54b4
SophMe <116891325+SophMe@users.noreply.github.com>
2024-01-10 12:54:53 +0100

    add fs.readFileSync

--- timer.js
+++ timer.js
@@ -1,34 +1,5 @@
 // timer
 // https://stackoverflow.com/questions/14249506/how-can-i-wait-in-node-js-javascript-l-need-to-pause-for-a-period-of-time
-async function timer() {
-  const startTime = new Date();
-  console.log(startTime.toLocaleTimeString());
-
-  const randomTime = Math.floor(Math.random() * 15000) + 1000;
-  const seconds = Math.floor(randomTime /1000);
-  const pluralSuffix = seconds === 1 ? '' : 's';
-
-  console.log(`Waiting for ${seconds} second${pluralSuffix}...`);
-
-  await sleep(seconds * 1000);
-  // console.log(seconds); 
-  const endTime = new Date();
-  const waitTime = (endTime - startTime) / 1000;
-  console.log(`Waiting time was ${waitTime} second${pluralSuffix}`);
-}
-
-function sleep(ms) {
-  return new Promise((resolve) => {
-    setTimeout(resolve, ms);
-  });
-}
-
-timer();
-
-// run in terminal with "node timer.js"
-
-// create file with random numbers
-// https://nodejs.org/en/learn/manipulating-files/writing-files-with-nodejs
 
 const fs = require('fs');
 
@@ -48,15 +19,26 @@ fs.writeFile('random_numbers.txt', content, (err) => {
   } else {
     console.log('File written successfully');
   }
-  console.log(content);
 });
 
-// fs.readFile('random_numbers.txt', 'utf8', (err, data) => {
-//   if (err) {
-//     console.error(err);
-//   } else {
-//     const numbersArray = data.split(',');
-//     const numberOfNumbers = numbersArray.length;
-//     console.log(`There are ${numberOfNumbers} numbers in the file: `);
-//   };
-// });
+// console.log(content);
+
+timer();
+
+function timer() {
+  const startTime = new Date();
+  console.log(startTime.toLocaleTimeString());
+
+  for (let i = 0; i < 100000; i++) {
+    fs.readFileSync('random_numbers.txt');
+  }
+
+  const endTime = new Date();
+  console.log(endTime.toLocaleTimeString());
+
+  const waitTime = (endTime - startTime) / 1000;
+  const pluralSuffix = waitTime === 1 ? '' : 's';
+  console.log(`Reading time was ${waitTime} second${pluralSuffix}`);
+}
+
+// run in terminal with "node timer.js"
