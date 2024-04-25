359ef8463aa1c8eb2eea9e506b9610d38e4519f3
SophMe <116891325+SophMe@users.noreply.github.com>
2024-04-25 13:01:28 +0200

    get unique cominations from both columns

--- 100 rows/bestPairs.js
+++ 100 rows/bestPairs.js
@@ -1,25 +1,34 @@
 const fs = require('fs');
 
-function readCSVFile() {
+function printUniqueCombs() {
     const filePath = 'bestPairs.csv';
+    const numRowsToPrint = 100;
+    const allLetters = [];
     fs.readFile(filePath, 'utf8', (err, data) => {
         if (err) {
             console.error('Error reading the CSV file:', err);
             return;
         }
 
-        const rows = data.split('\n');
-        const numRowsToPrint = 100;
-        for (let i = 0; i < Math.min(numRowsToPrint, rows.length); i++) {
-          const columns = rows[i].split(',');
-          if (columns.length >= 2 ) {
-            const firstChCol1 = columns[0].charAt(0);       // charAT retrieves the character at a specified index within the string
-            const firstChCol2 = columns[1].charAt(0);
-            // write data to terminal
-            process.stdout.write(firstChCol1 + firstChCol2 + '\n');       // process.stdout represents standard output stream 
-          }
+        const rows = data.split('\n').slice(0, numRowsToPrint);
+        for (let i = 0; i < rows.length; i++) {
+            const columns = rows[i].split(',');
+            if (columns.length > 1) {
+                const column1LettersOnly = columns[0].match(/[a-zA-Z]+/g);      // regular expression to get rid of numbers
+                const column2LettersOnly = columns[1].match(/[a-zA-Z]+/g);
+                if (column1LettersOnly) {
+                    allLetters.push(...column1LettersOnly);                     // push letters from column to allLetters array
+                }
+                if (column2LettersOnly) {
+                    allLetters.push(...column2LettersOnly);
+                }
+            }
         }
+
+        const uniqueCombs = [...new Set(allLetters)];                         // Set object can only contain unique elements
+
+        console.log('Unique combinations:', uniqueCombs.sort());
     });
 }
 
-readCSVFile();
+printUniqueCombs();
